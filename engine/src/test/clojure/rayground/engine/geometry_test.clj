67ee15b16152ceb12f9498d8114b171815388747
Stefan Holzmüller <stefanholzmueller@github>
2012-12-01 13:19:50 +0100

    wrote a macro to wrap Midje (fact ...) into (deftest ...) calls in order to run them with mvn test

--- engine/src/test/clojure/rayground/engine/geometry_test.clj
+++ engine/src/test/clojure/rayground/engine/geometry_test.clj
@@ -1,26 +1,35 @@
 (ns rayground.engine.geometry-test
   (:use rayground.engine.geometry)
+  (:use clojure.test)
   (:use midje.sweet)
   )
 
-(fact "assert vectors have exactly 3 elements"
+(defmacro fact2 [name & body]
+  `(deftest ~(symbol (clojure.string/replace name #"[^a-zA-Z0-9]" "_"))
+     (fact ~name
+       ~@body
+       )
+     )
+  )
+
+(fact2 "assert vectors have exactly 3 elements"
   (new-ray [] []) => (throws AssertionError)
   (new-ray [1 2 3] [1]) => (throws AssertionError)
   (new-ray [1 2 3] [1 2 3]) =not=> (throws AssertionError)
   )
 
-(fact "constructor fn creates a ray"
+(fact2 "constructor fn creates a ray"
   (let [r (new-ray [-1.0 -2.0 -3.0] [1 2 3])]
     (:origin r) => [-1.0 -2.0 -3.0]
     (:direction r) => [0.2672612419124244 0.5345224838248488 0.8017837257372732]
     )
   )
 
-(fact "ray uses (normalized) unit vector"
+(fact2 "ray uses (normalized) unit vector"
   (norm (:direction (new-ray [0 0 0] [1 2 3]))) => (roughly 1)
   )
 
-(fact "length of vectors"
+(fact2 "length of vectors"
   (norm [1 1 1]) => (Math/sqrt 3)
   (norm [2 0 0]) => 2.0
   (norm [1 2 2]) => 3.0
