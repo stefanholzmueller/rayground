24a1cc83c74d4face5b18acaed1d94eb7c878c5c
Stefan Holzmüller <stefanholzmueller@github>
2013-01-06 11:34:53 +0100

    refactored Midje wrappers

--- engine/src/test/clojure/rayground/tests.clj
+++ engine/src/test/clojure/rayground/tests.clj
@@ -6,20 +6,20 @@
 ; workarounds to wrap Midje (fact)s in clojure.test (deftest)s in order to run them with "mvn test"
 ; see https://groups.google.com/forum/?fromgroups=#!topic/clojure-maven-plugin/lPYV7MODO0g
 
-(defmacro midje-wrapper [name & body]
-  (let [deftest-name (gensym (clojure.string/replace name #"[^a-zA-Z0-9]" "_"))
-        midje-call (clojure.string/replace (first &form) #"2$" "")
+(defmacro wrap-midje [wrapper name & body]
+  (let [deftest-sym (gensym (str "test_" (clojure.string/replace name #"[^a-zA-Z0-9]" "_")))
+        midje-call (symbol (clojure.string/replace wrapper #"2$" ""))
         ]
-    (deftest deftest-name
-      (midje-call name body)
+    `(deftest ~deftest-sym
+      (~midje-call ~name ~@body)
       )
     )
   )
 
 (defmacro fact2 [name & body]
-  (midje-wrapper name body)
+  `(wrap-midje ~(first &form) ~name ~@body)
   )
 
 (defmacro future-fact2 [name & body]
-  (midje-wrapper name body)
+  `(wrap-midje ~(first &form) ~name ~@body)
   )
